8978dd0215208dc02f9f902ef8a0acada4ed5fe8
DilipSth <119152045+DilipSth@users.noreply.github.com>
2023-07-17 20:18:01 +0545

    Add files via upload

---
+++ JavaScript/productScript.js
@@ -0,0 +1,123 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "View more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var more1Text = document.getElementById("more1");
+    var btn1Text = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btn1Text.innerHTML = "View more"; 
+      more1Text.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btn1Text.innerHTML = "Read less"; 
+      more1Text.style.display = "inline";
+    }
+  }
+  function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var more2Text = document.getElementById("more2");
+    var btn2Text = document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btn2Text.innerHTML = "View more"; 
+      more2Text.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btn2Text.innerHTML = "Read less"; 
+      more2Text.style.display = "inline";
+    }
+  }
+  function myFunction3() {
+    var dots3 = document.getElementById("dots3");
+    var more3Text = document.getElementById("more3");
+    var btn3Text = document.getElementById("myBtn3");
+  
+    if (dots3.style.display === "none") {
+      dots3.style.display = "inline";
+      btn3Text.innerHTML = "View more"; 
+      more3Text.style.display = "none";
+    } else {
+      dots3.style.display = "none";
+      btn3Text.innerHTML = "Read less"; 
+      more3Text.style.display = "inline";
+    }
+  }
+  function myFunction4() {
+    var dots4 = document.getElementById("dots4");
+    var more4Text = document.getElementById("more4");
+    var btn4Text = document.getElementById("myBtn4");
+  
+    if (dots4.style.display === "none") {
+      dots4.style.display = "inline";
+      btn4Text.innerHTML = "View more"; 
+      more4Text.style.display = "none";
+    } else {
+      dots4.style.display = "none";
+      btn4Text.innerHTML = "Read less"; 
+      more4Text.style.display = "inline";
+    }
+  }
+  function myFunction5() {
+    var dots5 = document.getElementById("dots5");
+    var more5Text = document.getElementById("more5");
+    var btn5Text = document.getElementById("myBtn5");
+  
+    if (dots5.style.display === "none") {
+      dots5.style.display = "inline";
+      btn5Text.innerHTML = "View more"; 
+      more5Text.style.display = "none";
+    } else {
+      dots5.style.display = "none";
+      btn5Text.innerHTML = "Read less"; 
+      more5Text.style.display = "inline";
+    }
+  }
+  function myFunction6() {
+    var dot6 = document.getElementById("dots6");
+    var more6Text = document.getElementById("more6");
+    var btn6Text = document.getElementById("myBtn6");
+  
+    if (dots6.style.display === "none") {
+      dots6.style.display = "inline";
+      btn6Text.innerHTML = "View more"; 
+      more6Text.style.display = "none";
+    } else {
+      dots6.style.display = "none";
+      btn6Text.innerHTML = "Read less"; 
+      more6Text.style.display = "inline";
+    }
+  }
+  function myFunction7() {
+    var dots7 = document.getElementById("dots7");
+    var more7Text = document.getElementById("more7");
+    var btn7Text = document.getElementById("myBtn7");
+  
+    if (dots7.style.display === "none") {
+      dots7.style.display = "inline";
+      btn7Text.innerHTML = "View more"; 
+      more7Text.style.display = "none";
+    } else {
+      dots7.style.display = "none";
+      btn7Text.innerHTML = "Read less"; 
+      more7Text.style.display = "inline";
+    }
+  }
+  function addToCart() {
+alert("Product added to your cart!");
+}
